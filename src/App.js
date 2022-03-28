7b6a15c0702d10ac54f038681594e939b0c1d1b6
SatuPet <satupet@metropolia.fi>
2022-03-28 10:42:59 +0300

    loginNew

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Nav from './components/nav';
+import {MediaProvider} from './contexts/MediaContext';
 import Home from './views/home';
 import Login from './views/login';
+import Logout from './views/logout';
 import Profile from './views/profile';
 import Single from './views/single';
 
@@ -10,13 +12,16 @@ const App = () => {
   return (
     // eslint-disable-next-line no-undef
     <Router basename={process.env.PUBLIC_URL}>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/single" element={<Single />} />
-      </Routes>
+      <MediaProvider>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </MediaProvider>
     </Router>
   );
 };
